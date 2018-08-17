f566d664e01ea129997bc2e34334c9090e789df1
prabeshpd <prabesh1995nepaljtm@gmail.com>
2018-08-17 12:24:00 +0545

    Session variable were created for checking whether individuals admin, staff and employee are logged in or not before accessing their page.

---
+++ session.php
@@ -0,0 +1,104 @@
+<?php
+class Session{
+    private $admin_signed_in = false;
+    private $staff_signed_in = false;
+    private $employee_signed_in = false;
+    public $admin_user_id;
+    public $staff_user_id;
+    public $employee_user_id;
+    public $message;
+
+    function __construct(){
+        session_start();
+        $this->check_admin_login();
+        $this->check_staff_login();
+        $this->check_employee_login();
+        $this->check_message();
+    }
+
+    public function message($msg=""){
+        if(!empty($msg)){
+            $_SESSION['message'] = $msg;
+        }else{
+            return $message;
+        }
+    }
+    public function check_message(){
+        if(isset($_SESSION['message'])){
+            $this->message = $_SESSION['message'];
+            unset($_SESSION['message']);
+        }else{
+            $this->message = "";
+        }
+    }
+    public function is_admin_signed_in(){
+        return $this->admin_signed_in;
+    }
+    public function is_staff_signed_in(){
+        return $this->staff_signed_in;
+    }
+    public function is_employee_signed_in(){
+        return $this->employee_signed_in;
+    }
+    public function admin_login($user){
+        if($user){
+            $this->admin_user_id = $_SESSION['admin_user_id'] = $admin_user->admin_id;
+            $this->admin_signed_in = true;
+        }
+    }
+    public function admin_logout(){
+            unset($_SESSION['admin_user_id']);
+            $this->admin_signed_in = false;
+        }
+    
+    public function staff_login($user){
+        if($user){
+            $this->staff_user_id = $_SESSION['staff_user_id'] = $staff_user->staff_id;
+            $this->admin_signed_in = true;
+        }
+    }
+    public function staff_logout(){
+            unset($_SESSION['staff_user-id']);
+            $this->staff_signed_in = false;
+        }
+    
+    public function employee_login($user){
+        if($user){
+            $this->employee_user_id = $_SESSION['employeee_user_id'] = $employee_user->admin_id;
+            $this->employee_signed_in = true;
+        }
+    }
+    public function employee_logout(){
+            unset($_SESSION['employee_user-id']);
+            $this->employee_signed_in = false;
+        }
+    private function check_admin_login(){
+        if(isset($_SESSION['admin_user_id'])){
+            $this->admin_user_id = $_SESSION['admin_user_id'];
+            $this->admin_signed_in = true;
+        }else{
+            unset($this->admin_user_id);
+            $this->admin_signed_in = false;
+        }
+    }
+    private function check_staff_login(){
+        if(isset($_SESSION['staff_user_id'])){
+            $this->staff_user_id = $_SESSION['staff_user_id'];
+            $this->staff_signed_in = true;
+        }else{
+            unset($this->staff_user_id);
+            $this->staff_signed_in = false;
+        }
+    }
+    private function check_employee_login(){
+        if(isset($_SESSION['employee_user_id'])){
+            $this->employee_user_id = $_SESSION['employee_user_id'];
+            $this->employee_signed_in = true;
+        }else{
+            unset($this->employee_user_id);
+            $this->employee_signed_in = false;
+        }
+    }
+}
+$session = new Session();
+?>
