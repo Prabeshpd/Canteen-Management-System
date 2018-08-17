618117859e6867c4dc2b0347032a5c0e614bb676
prabeshpd <prabesh1995nepaljtm@gmail.com>
2018-08-17 12:58:05 +0545

    Employee_user and Staff_user table were created and logics to check whether the users are from database data or not were implemented

---
+++ employeeuser.php
@@ -0,0 +1,24 @@
+<?php
+class Employeeuser extends Db_object{
+    protected static $db_table = "employee_user";
+    protected static $db_table_fields = array('first_name', 'last_name', 'email', 'password');
+    public $employee_id;
+    public $first_name;
+    public $last_name;
+    public $email;
+    public $password;
+
+    public function verify_emloyee_user($email, $password){
+        global $database;
+        $email = $database->escape_string($email);
+        $password = $database->escape_string($password);
+        $sql = "SELECT FROM " . self::$db_table . " WHERE ";
+        $sql .= "email = '{email}' ";
+        $sql .= "And password = '{password}' ";
+        $the_result_array = self::find_by_query($sql);
+        return !empty($the_result_array) ? array_shift($the_result_array) : false;
+    }
+}
+
+
+?>
---
+++ staffuser.php
@@ -0,0 +1,24 @@
+<?php
+class Staffuser extends Db_object{
+    protected static $db_table = "staff_user";
+    protected static $db_table_fields = array('first_name', 'last_name', 'email', 'password');
+    public $staff_id;
+    public $first_name;
+    public $last_name;
+    public $email;
+    public $password;
+
+    public function verify_staff_user($email, $password){
+        global $database;
+        $email = $database->escape_string($email);
+        $password = $database->escape_string($password);
+        $sql = "SELECT FROM " . self::$db_table . " WHERE ";
+        $sql .= "email = '{email}' ";
+        $sql .= "And password = '{password}' ";
+        $the_result_array = self::find_by_query($sql);
+        return !empty($the_result_array) ? array_shift($the_result_array) : false;
+    }
+}
+
+
+?>
