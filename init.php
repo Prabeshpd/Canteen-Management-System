38ff42aa6a5c357805edfa1e48b81e1a0de5087a
prabeshpd <prabesh1995nepaljtm@gmail.com>
2018-08-17 13:06:37 +0545

    All files were included in init.php so, we can include necessary files easily and functions.php was created for creating routing easy and Spl_autoload function was implemented to include all the pages

---
+++ init.php
@@ -0,0 +1,9 @@
+<?php
+require_once("config.php");
+require_once("database.php");
+require_once("db_object.php");
+require_once("session.php");
+require_once("adminuser.php");
+require_once("staffuser.php");
+require_once("employeeuser.php");
+?>
